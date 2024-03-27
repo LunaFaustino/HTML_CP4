e6f8a456419c7c26ad831f9dcd2abff3c8e9e5ae
LunaFaustino <lu.faustino2000@gmail.com>
2024-03-26 22:48:14 -0300

    Footer com os nomes e RMs

---
+++ src/app/Footer/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+        <>
+        <footer>
+            <span>Luna Faustino Lima - RM552473</span>
+            <br />
+            <span>Larissa Araújo Gama Alvarenga - RM96496</span>
+        </footer>
+        </>
+    )
+}
+
+export default Footer;
